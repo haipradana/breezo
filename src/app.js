e8cc54a7fd7d4458e23f56258caa27f53bf69815
Pradana Yahya Abdillah <you@example.com>
2025-01-17 19:49:11 +0700

    Add: Back to top

--- src/app.js
+++ src/app.js
@@ -2,11 +2,16 @@
 window.onscroll = function() {
     const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
+    const toTop = document.querySelector('#back-to-top');
 
     if(window.scrollY > fixedNav){
         header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
     }else {
         header.classList.remove('navbar-fixed');
+        toTop.classList.add('hidden');
+        toTop.classList.remove('flex');
     }
 };
 
@@ -17,4 +22,12 @@ const nav = document.querySelector('#nav-menu');
 hamburger.addEventListener('click', function(){
     hamburger.classList.toggle('hamburger-active');
     nav.classList.toggle('hidden');
+});
+
+// Close hamburger without click hamburger
+window.addEventListener('click', function(e){
+    if( e.target != nav && e.target != hamburger){
+        hamburger.classList.remove('hamburger-active');
+        nav.classList.add('hidden');
+    }
 });
